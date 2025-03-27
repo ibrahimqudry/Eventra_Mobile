93c4755c6afaea3e4c8e9a62114adf4cfb107945
Miramaged802 <miramaged345@gmail.com>
2025-03-28 01:53:24 +0200

    mira's work last edit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import SplashScreen from './screens/SplashScreen';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import HomeScreen from './screens/HomeScreen';
+import EventsScreen from './screens/EventsScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,7 +15,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="Register" component={Register} /> 
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Event" component={EventsScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>

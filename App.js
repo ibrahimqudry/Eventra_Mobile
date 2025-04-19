ae258a2e896e3aaae1682e1c095f98dc8ad65ad4
ibrahimqudry <ibrahimqudry@gmail.com>
2025-04-19 22:10:02 +0200

    merging mira and reem branches to the main branch

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="TestPayment"
+        initialRouteName="Home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Splash" component={SplashScreen} />
